2169df12a5824aa134ffdaa9f9d8710a5a95436d
Your Name <jyotiverma@apptunix.com>
2022-08-19 19:37:31 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl,FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'form';
+
+  loginForm=new FormGroup({
+    sn: new FormControl('',[Validators.required]),
+    title: new FormControl({ value: '', disabled: this.isDisabled },[Validators.required]),
+    price: new FormControl({ value: '', disabled: this.isDisabled },[Validators.required]),
+    rating: new FormControl({value:'',disabled:true})
+  })
+  
+  set isDisabled(value: boolean) {
+    this.isDisabled = value;
+    if(value) {
+     this.loginForm.controls['title'].disable();
+    } else {
+       this.loginForm.controls['price'].enable();
+     }
+   }
+  //  this.loginForm.get('myCheckbox').valueChanges
+    // .subscribe((value: any) => {
+      // if(value) {
+        // this.loginForm.get('myEmailField').setValidators(Validators.required);
+      // } else {
+        // this.myForm.get('myEmailField').clearValidators();
+      // })
+    // }
+  
+  submitted: boolean = false;
+  loginUser(){
+    this.submitted =true;
+    console.log(this.loginForm.value);
+      this.loginForm.reset();
+  }
+   get sn(){
+    return this.loginForm.get('sn');
+  }
+  get title(){
+    return this.loginForm.get('title');
+  }
+  get price(){
+    return this.loginForm.get('price');
+  }
+
 }
